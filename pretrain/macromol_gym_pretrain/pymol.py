d31d9fff77d31578be0b35825f3173e26e474c54
Kale Kundert <kale@thekunderts.net>
2024-12-08 20:41:02 -0500

    feat: move pymol functions into the right packages

--- pretrain/macromol_gym_pretrain/pymol.py
+++ pretrain/macromol_gym_pretrain/pymol.py
@@ -1,9 +1,7 @@
 import pymol
-import macromol_gym_pretrain as mmgp
 import macromol_dataframe as mmdf
 import numpy as np
 import os
-import re
 
 from pymol import cmd
 from pymol.wizard import Wizard
@@ -26,9 +24,7 @@ from macromol_voxelize.pymol import (
 from macromol_dataframe import (
         make_coord_frame, invert_coord_frame, get_origin,
 )
-from contextlib import contextmanager
 from pipeline_func import f
-from math import pi
 
 class TrainingExamples(Wizard):
 
@@ -510,134 +506,5 @@ def get_frame_names(directions):
         names.append(name)
 
     return names
-
-
-def _sum_occ(sele):
-    with sele_or_pseudoatom(sele) as sele:
-        counter = {'q': 0}
-        cmd.iterate(sele, 'counter["q"] += q', space=locals())
-        return counter['q']
-
-def sum_occ(sele):
-    print(_sum_occ(sele), 'atoms')
-
-pymol.cmd.extend('sum_occ', sum_occ)
-cmd.auto_arg[0]['sum_occ'] = cmd.auto_arg[0]['zoom']
-
-def _density(center, radius_A):
-    with sele_or_pseudoatom(center) as sele:
-        radius_nm = float(radius_A) / 10
-        volume_nm3 = 4/3 * pi * radius_nm**3
-        occupancy = _sum_occ(f'all within {radius_A} of ({sele})')
-        return occupancy / volume_nm3
-
-def density(center, radius_A):
-    cmd.iterate_state(0, center, 'print(f"{x:.3f} {y:.3f} {z:.3f}")')
-    print(_density(center, radius_A), 'atoms/nm^3')
-
-pymol.cmd.extend('density', density)
-cmd.auto_arg[0]['density'] = cmd.auto_arg[0]['zoom']
-
-def show_centers(spacing_A=10, density_target_atoms_nm3=35, density_radius_A=15, sele='all', state=0):
-    cmd.delete('zone_centers')
-
-    spacing_A = float(spacing_A)
-    density_target_atoms_nm3 = float(density_target_atoms_nm3)
-    density_radius_A = float(density_radius_A)
-
-    atoms = mmdf.from_pymol(sele, state)
-    zone_centers_A = mmgp.calc_zone_centers_A(atoms, spacing_A)
-
-    centers_above_density_target = 0
-
-    for i, center_A in enumerate(zone_centers_A):
-        density_atoms_nm3 = _density(center_A, density_radius_A)
-
-        if density_atoms_nm3 >= density_target_atoms_nm3:
-            centers_above_density_target += 1
-            color = 'yellow'
-            print(i, center_A)
-        else:
-            shade = 10 * int(10 * density_atoms_nm3 / density_target_atoms_nm3)
-            color = f'gray{shade}'
-
-        cmd.pseudoatom(
-                'zone_centers',
-                pos=repr(list(center_A)),
-                resi=i,
-                color=color,
-        )
-
-    print(f"Centers with >{density_target_atoms_nm3} atoms/nm^3: {centers_above_density_target}")
-
-pymol.cmd.extend('show_centers', show_centers)
-
-def show_neighbors(sele='sele', geometry='icosahedron faces', distance_A=30, density_target_atoms_nm3=35, density_radius_A=15, state=0):
-    cmd.delete('neighbor_centers')
-
-    distance_A = float(distance_A)
-    density_target_atoms_nm3 = float(density_target_atoms_nm3)
-    density_radius_A = float(density_radius_A)
-
-    neighbors = mmgp.find_neighbor_centers_A(geometry, distance_A)
-
-    center_A = np.zeros(3)
-    cmd.iterate_state(state, sele, 'center_A[:] = (x, y, z)', space=locals())
-
-    neighbors_above_density_target = 0
-
-    for i, offset_A in enumerate(neighbors):
-        neighbor_A = center_A + offset_A
-        density_atoms_nm3 = _density(neighbor_A, density_radius_A)
-
-        if density_atoms_nm3 >= density_target_atoms_nm3:
-            neighbors_above_density_target += 1
-            color = 'yellow'
-            print(i, neighbor_A)
-        else:
-            shade = 10 * int(10 * density_atoms_nm3 / density_target_atoms_nm3)
-            color = f'gray{shade}'
-
-        cmd.pseudoatom(
-                'neighbor_centers',
-                pos=repr(list(neighbor_A)),
-                resi=i,
-                color=color,
-        )
-
-    print(f"Neighbors with >{density_target_atoms_nm3} atoms/nm^3: {neighbors_above_density_target}")
-
-pymol.cmd.extend('show_neighbors', show_neighbors)
-cmd.auto_arg[0]['show_neighbors'] = cmd.auto_arg[0]['zoom']
-
-@contextmanager
-def sele_or_pseudoatom(sele_or_xyz):
-    if isinstance(sele_or_xyz, str):
-        sele = sele_or_xyz
-        yield sele
-    else:
-        xyz = sele_or_xyz
-        with tmp_pseudoatom(xyz) as sele:
-            yield sele
-
-@contextmanager
-def tmp_pseudoatom(xyz):
-    name = '__xyz'
-    names = cmd.get_names('all')
-
-    if name in names:
-        pattern = fr'^{name}(\d+)$'
-        i = 1 + max(
-                (m.group(1) for x in names if (m := re.match(pattern, x))),
-                default=0,
-        )
-        name = f'{name}{i}'
-
-    cmd.pseudoatom(name, pos=list(xyz))
-
-    try:
-        yield name
-    finally:
-        cmd.delete(name)
 
 
