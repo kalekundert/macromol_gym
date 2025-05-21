698b4a3f96f656947515c0631141f02c9633f271
Kale Kundert <kale@thekunderts.net>
2025-05-21 14:53:08 -0400

    chore: bump version

--- pretrain/macromol_gym_pretrain/__init__.py
+++ pretrain/macromol_gym_pretrain/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from macromol_gym_unsupervised import *
 from .neighbors import *
--- unsupervised/macromol_gym_unsupervised/__init__.py
+++ unsupervised/macromol_gym_unsupervised/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .database_io import *
 from .images import *
